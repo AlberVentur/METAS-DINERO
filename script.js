70e446fd13edc73ba11a29477d3f45fb13e4a921
ventas <154559613+AlberVentur@users.noreply.github.com>
2024-05-11 23:02:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,32 @@
-let ahorros = JSON.parse(localStorage.getItem('ahorros')) || {};
-
-function guardarAhorro() {
-  const selectedDay = document.getElementById('daySelect').value;
-  const amount = parseInt(document.getElementById('amount').value);
-  if (!ahorros[selectedDay]) {
-    ahorros[selectedDay] = 0;
-  }
-  ahorros[selectedDay] += amount;
-  actualizarLocalStorage();
-  document.getElementById('amount').value = '';
-}
-
-function verTotal() {
-  mostrarAhorros();
-}
-
-function eliminarTodo() {
-  ahorros = {};
-  actualizarLocalStorage();
-  mostrarAhorros();
-}
-
-function actualizarLocalStorage() {
-  localStorage.setItem('ahorros', JSON.stringify(ahorros));
-}
-
-function mostrarAhorros() {
-  const ahorroList = document.getElementById('ahorroList');
-  ahorroList.innerHTML = '';
-  let total = 0;
-  for (const day in ahorros) {
-    const amount = ahorros[day];
-    total += amount;
-    const listItem = document.createElement('li');
-    listItem.textContent = `${day}: $${amount}`;
-    ahorroList.appendChild(listItem);
-  }
-  document.getElementById('totalAmount').textContent = `Total Ahorrado: $${total}`;
-}
-
-function ocultarAhorros() {
-  document.getElementById('totalAmount').style.display = 'none';
-}
-
-ocultarAhorros(); // Ocultar el acumulado al inicio
+document.addEventListener("DOMContentLoaded", function() {
+    const addToCartButtons = document.querySelectorAll(".add-to-cart");
+    const buyButton = document.querySelector(".buy-button");
+  
+    addToCartButtons.forEach(button => {
+      button.addEventListener("click", addToCart);
+    });
+  
+    buyButton.addEventListener("click", goToWhatsApp);
+  
+    function addToCart(event) {
+      const product = event.target.closest(".product");
+      const productName = product.querySelector("h2").textContent;
+      const productPrice = product.querySelector(".price").textContent;
+      
+      const cartItems = document.querySelector(".cart-items");
+      const newItem = document.createElement("li");
+      newItem.classList.add("cart-item");
+      newItem.textContent = `${productName} - ${productPrice}`;
+      cartItems.appendChild(newItem);
+    }
+  
+    function goToWhatsApp() {
+      // Agregar tu número de WhatsApp aquí
+      const phoneNumber = "tu_numero_de_whatsapp";
+      // Crear el enlace de WhatsApp con el mensaje y el número de teléfono
+      const whatsappLink = `https://wa.me/${phoneNumber}?text=Hola,%20quiero%20comprar%20los%20siguientes%20productos:%0A`;
+      // Redireccionar a WhatsApp
+      window.location.href = whatsappLink;
+    }
+  });
+  
